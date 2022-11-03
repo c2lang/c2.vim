78a4e17b6c3c7303c313a69e8b91794e964fd2bb
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2022-11-03 06:25:38 +0100

    lua: cleanup

--- ftplugin/c2.vim
+++ ftplugin/c2.vim
@@ -1,20 +1,8 @@
 " Only do this when not done yet for this buffer
 
-"compiler c2c
-
-"setlocal suffixesadd=.c2
-"setlocal commentstring=//\ %s
-"setlocal makeprg=c2c
+setlocal makeprg=c2c
 
 let g:c2vim_debug = get(g:, 'c2vim_debug', 0)
-
-" This forces a reload of the plugin when we source it.
-" Helps development times tremendously
-
-lua<<EOF
--- uncomment below to allow "source %" to reload the plugin during development
--- require('plenary.reload').reload_module('c2vim', true)
-EOF
 
 lua c2vim = require("c2vim")
 
--- lua/c2vim/init.lua
+++ lua/c2vim/init.lua
@@ -1,5 +1,4 @@
-local scan = require("plenary.scandir")
-utils = require("c2vim.utils")
+utils = require('c2vim.utils')
 
 local M = {}
 
@@ -16,31 +15,18 @@ end
 
 function split(s, delimiter)
     result = {}
-    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+    for match in (s..delimiter):gmatch('(.-)'..delimiter) do
         table.insert(result, match)
     end
     return result
-end
-
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
 end
 
 function get_tag(cmd)
     res = os.capture(cmd, false)
     --print(res)
     local result = split(res, ' ')
-    if (result[1] == "no") then
-        print("No symbol found")
+    if (result[1] == 'no') then
+        print('No symbol found')
         return
     end
 
@@ -48,7 +34,7 @@ function get_tag(cmd)
     local dest_line = result[3] + 0
     local dest_col = result[4] - 1
     --print("FOUND: ".. dest_file .. " " .. dest_line .. " " .. dest_col )
-    print(" "); -- otherwise shows :C2TagResult
+    print(' '); -- otherwise shows :C2TagResult
     vim.cmd('e ' .. dest_file)
     vim.cmd('set so=999')
     vim.api.nvim_win_set_cursor(0, { dest_line, dest_col })
@@ -59,19 +45,20 @@ function M.getTagDef()
     local line,col = unpack(vim.api.nvim_win_get_cursor(0))
     col = col + 1   -- col seems to be 0-based
     local filename = vim.api.nvim_eval('expand("%")')
-    --local cwd = vim.fn.getcwd()
-    --local filename = string.sub(current_file, string.len(cwd) + 2)
 
-    local cmd = "c2tags " .. filename .. " " .. line .. " " .. col
+    local cmd = 'c2tags ' .. filename .. ' ' .. line .. ' ' .. col
     get_tag(cmd)
 end
 
 function M.getSymbolDef(symbol)
-    -- word under cursor:
-    -- local symbol = vim.api.nvim_eval('expand("<cfile>")')
-
-    local cmd = "c2tags " .. symbol
+    local cmd = 'c2tags ' .. symbol
     get_tag(cmd)
 end
 
 return M
+
+-- local cwd = vim.fn.getcwd()
+-- local filename = string.sub(current_file, string.len(cwd) + 2)
+-- word under cursor:
+-- local symbol = vim.api.nvim_eval('expand("<cfile>")')
+
